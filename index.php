d2a8736a183b2d013e51339f3be827565199aa4b
Alexander Schmidt <alexander.schmidt@edvas.de>
2015-02-25 13:55:02 +0100

    dev

--- index.php
+++ index.php
@@ -1,6 +1,33 @@
 <?php defined( '_JEXEC' ) or die; 
 
-include_once JPATH_THEMES.'/'.$this->template.'/logic.php';
+// include_once JPATH_THEMES.'/'.$this->template.'/logic.php';
+
+// variables
+$app = JFactory::getApplication();
+$doc = JFactory::getDocument(); 
+$menu = $app->getMenu();
+$active = $app->getMenu()->getActive();
+$params = $app->getParams();
+$pageclass = $params->get('pageclass_sfx');
+$tpath = $this->baseurl.'/templates/'.$this->template;
+$templateparams	= $app->getTemplate(true)->params;
+
+// generator tag
+$this->setGenerator(null);
+
+// force latest IE & chrome frame
+$doc->setMetadata('x-ua-compatible','IE=edge,chrome=1');
+
+// js
+JHtml::_('jquery.framework');
+$doc->addScript($tpath.'/js/bootstrap.min.js');
+$doc->addScript($tpath.'/js/logic.js'); // <- use for custom script
+
+// css
+$doc->addStyleSheet($tpath.'/css/template.css');
+$doc->addStyleSheet(JUri::base().'/media/system/css/system.css');
+$doc->addStyleSheet(JPATH_THEMES.'/system/css/system.css');
+$doc->addStyleSheet(JPATH_THEMES.'/system/css/general.css');
 
 ?><!doctype html>
 
@@ -9,20 +36,38 @@ include_once JPATH_THEMES.'/'.$this->template.'/logic.php';
 <head>
 	<jdoc:include type="head" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+	<style type="text/stylesheet">
+		@-webkit-viewport   { width: device-width; }
+		@-moz-viewport      { width: device-width; }
+		@-ms-viewport       { width: device-width; }
+		@-o-viewport        { width: device-width; }
+		@viewport           { width: device-width; }
+	</style>
+	<script type="text/javascript">
+		//<![CDATA[
+		if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+			var msViewportStyle = document.createElement("style");
+			msViewportStyle.appendChild(
+				document.createTextNode("@-ms-viewport{width:auto!important}")
+			);
+			document.getElementsByTagName("head")[0].appendChild(msViewportStyle);
+		}
+		//]]>
+	</script>
+	<meta name="HandheldFriendly" content="true"/>
+	<meta name="apple-mobile-web-app-capable" content="YES"/>
 	<link rel="apple-touch-icon-precomposed" href="<?php echo $tpath; ?>/images/apple-touch-icon-57x57-precomposed.png">
 	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo $tpath; ?>/images/apple-touch-icon-72x72-precomposed.png">
 	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo $tpath; ?>/images/apple-touch-icon-114x114-precomposed.png">
 	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo $tpath; ?>/images/apple-touch-icon-144x144-precomposed.png">
 	<!-- Le HTML5 shim and media query for IE8 support -->
 	<!--[if lt IE 9]>
-	<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-	<script type="text/javascript" src="<?php echo $tpath; ?>/js/respond.min.js"></script>
+		<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<script type="text/javascript" src="<?php echo $tpath; ?>/js/respond.min.js"></script>
 	<![endif]-->
 </head>
   
 <body class="<?php echo (($menu->getActive() == $menu->getDefault()) ? ('front') : ('site')).' '.$active->alias.' '.$pageclass; ?>" role="document">
-
-	<?php require_once 'html/bootstrap.test.php'; // test only - delete this line ?>
 
 	<!-- 
 		YOUR CODE HERE
--- logic.php
+++ logic.php
@@ -22,9 +22,36 @@ $doc->addScript($tpath.'/js/bootstrap.min.js');
 $doc->addScript($tpath.'/js/logic.js'); // <- use for custom script
 
 // css
-if ($templateparams->get('runless', 1) == 1)
-{
-	require 'runless.php';
-}
+
+// begin function compress
+// function compress($buffer) 
+// {
+// 	$buffer = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $buffer);
+// 	$buffer = str_replace(array("\r\n","\r","\n","\t",'  ','    ','    '),'',$buffer);
+// 	$buffer = str_replace('{ ', '{', $buffer);
+// 	$buffer = str_replace(' }', '}', $buffer);
+// 	$buffer = str_replace('; ', ';', $buffer);
+// 	$buffer = str_replace(', ', ',', $buffer);
+// 	$buffer = str_replace(' {', '{', $buffer);
+// 	$buffer = str_replace('} ', '}', $buffer);
+// 	$buffer = str_replace(': ', ':', $buffer);
+// 	$buffer = str_replace(' ,', ',', $buffer);
+// 	$buffer = str_replace(' ;', ';', $buffer);
+// 	$buffer = str_replace(';}', '}', $buffer);
+// 	return $buffer;
+// }
+
+// merge css
+// $merged = file_get_contents($tpath.'/css/template.css');
+// $merged .= file_get_contents(JUri::base().'/media/system/css/system.css');
+// $merged .= file_get_contents(JPATH_THEMES.'/system/css/system.css');
+// $merged .= file_get_contents(JPATH_THEMES.'/system/css/general.css');
+
+// $compressed = compress($merged);
+
+// file_put_contents($tpath.'/template.css', $compressed);
 
 $doc->addStyleSheet($tpath.'/css/template.css');
+$doc->addStyleSheet(JUri::base().'/media/system/css/system.css');
+$doc->addStyleSheet(JPATH_THEMES.'/system/css/system.css');
+$doc->addStyleSheet(JPATH_THEMES.'/system/css/general.css');
