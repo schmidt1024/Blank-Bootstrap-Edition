ad6080fafed3f9c9c44a65d3aeebaf1fddc532da
Alexander Schmidt <alexander.schmidt@edvas.de>
2019-05-03 16:53:41 +0200

    update to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,38 @@
 // VARIABLES
-const gulp          = require('gulp');
-const concat        = require('gulp-concat');
-const sass          = require('gulp-sass');
-const postcss       = require('gulp-postcss');
-const autoprefixer  = require('autoprefixer');
-const flexboxfixer  = require('postcss-flexboxfixer');
-const uglify        = require('gulp-uglify');
-const browserSync   = require('browser-sync').create();
-const zip           = require('gulp-zip');
+const { watch, series, src, dest } = require('gulp');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const flexboxfixer = require('postcss-flexboxfixer');
+const uglify = require('gulp-uglify');
+const browserSync = require('browser-sync').create();
+const zip = require('gulp-zip');
 
 // FILES
-gulp.task('files', function() {
-    var bootstrap = gulp.src('node_modules/bootstrap/scss/**/*')
-        .pipe(gulp.dest('scss/bootstrap'));
-    var fontawesome = gulp.src('node_modules/@fortawesome/fontawesome-free/scss/**/*')
-        .pipe(gulp.dest('scss/fontawesome'));
-    var webfonts = gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/**/*')
-        .pipe(gulp.dest('webfonts'));
+function getFiles() {
+    let bootstrap = src('node_modules/bootstrap/scss/**/*')
+        .pipe(dest('scss/bootstrap/'));
+    let fontawesome = src('node_modules/@fortawesome/fontawesome-free/scss/**/*')
+        .pipe(dest('scss/fontawesome/'));
+    let webfonts = src('node_modules/@fortawesome/fontawesome-free/webfonts/**/*')
+        .pipe(dest('webfonts/'));
     return (bootstrap, fontawesome, webfonts);
-});
+}
 
 // CSS
-gulp.task('sass', function() {
-    return gulp.src(['scss/main.scss'])
+function prepareSass() {
+    return src(['scss/main.scss'])
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(postcss([ autoprefixer() ]))
         .pipe(postcss([ flexboxfixer() ]))
-        .pipe(gulp.dest('build'))
+        .pipe(dest('build/'))
         .pipe(browserSync.stream());
-});
+}
 
 // JS
-gulp.task('js', function() {
-    return gulp.src([
+function uglifyJavascript() {
+    return src([
         'node_modules/jquery/dist/jquery.slim.min.js',
         'node_modules/popper.js/dist/umd/popper.min.js',
         'node_modules/bootstrap/dist/js/bootstrap.min.js',
@@ -40,32 +40,38 @@ gulp.task('js', function() {
         ])
         .pipe(concat('app.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('build'))
+        .pipe(dest('build/'))
         .pipe(browserSync.stream());
-});
+}
+
+// ZIP
+function zipTemplate() {
+    return src([
+        '**/*',
+        '!node_modules/**',
+        '!dist/**'
+        ])
+        .pipe(zip('frontend.zip'))
+        .pipe(dest('../'));
+}
 
 // SERVE
-gulp.task('serve', function() {
+function serveSite() {
     browserSync.init({
         // server: './'' // default server
         // proxy: 'http://localhost:8888/' // mamp
         proxy: 'http://localhost/your-website/' // usualy
     });
-    gulp.watch('js/**/*.js', gulp.series('js', 'zip'));
-    gulp.watch('scss/**/*.scss', gulp.series('sass', 'zip'));
-    gulp.watch('**/*.php', gulp.series('zip')).on('change', browserSync.reload);
-});
+    watch('js/**/*.js', series(uglifyJavascript, zipTemplate));
+    watch('scss/**/*.scss', series(prepareSass, zipTemplate));
+    watch('**/*.php', series(zipTemplate)).on('change', browserSync.reload);
+}
 
-// ZIP
-gulp.task('zip', function() {
-    return gulp.src([
-      '**/*',
-      '!node_modules/**',
-      '!dist/**'
-      ])
-      .pipe(zip('frontend.zip'))
-      .pipe(gulp.dest('../'));
-});
-
-gulp.task('default', gulp.series('files','sass','js','zip','serve'));
-
+// DEFAULT
+exports.default = series(
+    getFiles,
+    prepareSass,
+    uglifyJavascript,
+    zipTemplate,
+    serveSite
+);
